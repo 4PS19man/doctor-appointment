fbcd1e167fe04238015dddef21881db0bcad25a2
4PS19man <monikabhagyavathi@gmail.com>
2025-05-19 00:00:26 +0530

    modified main.ts for deployment

--- web-api/src/main.ts
+++ web-api/src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   const port = process.env.PORT || 3001;
   await app.listen(port, '0.0.0.0');
 
-  Logger.log(`🚀 Application is running on: http://api.schedula.localhost:${port}/${globalPrefix}`);
+  Logger.log(`Application is running on: http://api.schedula.localhost:${port}/${globalPrefix}`);
 }
 
 bootstrap();
